3c2ac6c45c0783b586ffbc49df5fc3780cdbddcf
PrestonFr <PrestonFr@gmail.com>
2023-03-01 18:18:37 -0500

    update route completion

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -15,6 +15,19 @@ user.get('/', async (req, res) => {
     }
   });
 
+// Find specific user
+user.get('/:id', async (req, res) => {
+    try {
+        const foundUser = await User.findOne({
+            where: { user_id: req.params.id }
+        })
+        res.status(200).json(foundUser)
+        console.log("Nice Work")
+    } catch (error) {
+        res.status(500).json(error)
+        console.log(error)
+    }
+})
 
 // Create user 
 user.post("/", async(req, res) => {
@@ -29,5 +42,36 @@ user.post("/", async(req, res) => {
     }
 })
 
+// Update user
+user.put('/:id', async (req, res) => {
+    try {
+        const updatedUser = await User.findByIdAndUpdate(req.params.id, { 
+            // $set - replaces the value with new value 
+            $set: req.body,
+            where: { user_id: req.params.id } });
+        res.status(200).json(updatedUser);
+    } catch(err) {
+        res.status(500).json(err)
+        console.log("broke")
+    }
+});
+
+
+// // Delete user 
+// user.delete('/:id', async (req, res) => {
+//     try {
+//         const deletedUser = await User.destroy({
+//             where: {
+//                 user_id: req.params.id
+//             }
+//         })
+//         res.status(200).json({
+//             message: `Successfully deleted`
+//         })
+//     } catch(err) {
+//         res.status(500).json(err)
+//         console.log("broken")
+//     }
+// })
 
 module.exports = user
